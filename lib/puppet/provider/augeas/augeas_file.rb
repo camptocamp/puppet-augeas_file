b34a15d87af278d589434c61feb3d846a1aa02f6
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-07-29 14:29:32 +0200

    Debug message for augeas resource

--- lib/puppet/provider/augeas/augeas_file.rb
+++ lib/puppet/provider/augeas/augeas_file.rb
@@ -1,10 +1,13 @@
 Puppet::Type.type(:augeas).provide(:augeas_file, :parent => Puppet::Type.type(:augeas).provider(:augeas)) do
   confine :feature => :augeas
+  # Hijack the augeas provider
+  defaultfor :feature => :augeas
 
   def need_to_run?
     # Managed by the augeas_file resource
-    resource.catalog.resources.select { |r|
+    resource.catalog.resources.each { |r|
       if r.is_a?(Puppet::Type.type(:augeas_file)) && r[:path] == resource[:incl]
+        Puppet.debug("Not applying individual augeas resource '#{resource[:name]}' as there is a matching augeas_file")
         return false
       end
     }
